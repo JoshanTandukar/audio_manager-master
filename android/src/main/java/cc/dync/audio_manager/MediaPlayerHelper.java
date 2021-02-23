50b10e21242fa182d0660ed3146f1b1ff2eb0545
JoshanPC <joshan.tandukar@yahoo.com>
2021-02-23 10:43:06 +0545

    Volume Manager

--- android/src/main/java/cc/dync/audio_manager/MediaPlayerHelper.java
+++ android/src/main/java/cc/dync/audio_manager/MediaPlayerHelper.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.MediaPlayer;
-import android.os.PowerManager;
 import android.util.Log;
 
 public class MediaPlayerHelper {
